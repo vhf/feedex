6ff38a4d99e96fca13363d68c2cff3c9095b6073
Victor Felder <victorfelder@gmail.com>
2016-02-14 00:27:15 +0100

    Remove a useless dep

--- mix.exs
+++ mix.exs
@@ -15,7 +15,6 @@ defmodule FeedEx.Mixfile do
   def application do
     [applications: [
       :logger,
-      :httpoison,
       :feedparser]]
   end
 
@@ -30,7 +29,6 @@ defmodule FeedEx.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:feedparser, github: "vhf/feedparser"},
-     {:httpoison, "~> 0.8.1"},
      {:dogma, "~> 0.0", only: :dev},
      {:credo, "~> 0.3", only: [:dev, :test]}]
   end
